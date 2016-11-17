05acd539418d3a1a838eb89fc8eea83f26088216
Maxene Clavio <clavio.maxene@gmail.com>
2016-11-17 10:17:07 -0800

    :)))

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,8 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function Butterfly(){
+  alert("Reset")
+}
+
+butterly()
