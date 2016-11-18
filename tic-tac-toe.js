7342bc29fea6cde7a7f3ea24c51c02e2b938deac
Maxene Clavio <clavio.maxene@gmail.com>
2016-11-18 10:17:22 -0800

    :))))

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,11 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-function Butterfly(){
-  alert("Reset")
+function butterfly(){
+  var circle = document.createElementNS(namespace ,"circle")
+  circle.setAttribute("cy" , 50)
+  circle.setAttribute("cx" , 50)
+  circle.setAttribute("r" , 30)
+  var butterfly = document.getElementById("game-board")
+atom.appendChild(child)
 }
-
-butterly()
